af1397fe79c5cde33e76ff3fa605fea52ea10d66
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-04 02:54:30 +0530

    concept of keys, and assigning unique id to components

--- course-project/src/App.js
+++ course-project/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => [...prevExpenses, expense]);
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   };
 
   // return React.createElement(
