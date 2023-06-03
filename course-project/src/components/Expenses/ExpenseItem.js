2f17f0a62c04846f89b6f8022ab547658e16af65
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-03 16:27:47 +0530

    using the state

--- course-project/src/components/Expenses/ExpenseItem.js
+++ course-project/src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,14 @@
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    title = "Updated";
+    setTitle("Updated");
+    console.log(title);
   };
 
   return (
