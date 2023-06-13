a42688051fb3e09b84f2facd33d7bff221b4bec1
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-13 20:14:43 +0530

    creating a wrapper component

--- fragments-portals-refs/src/components/Users/AddUser.js
+++ fragments-portals-refs/src/components/Users/AddUser.js
@@ -5,6 +5,7 @@ import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
 import classes from "./AddUser.module.css";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -45,7 +46,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -74,7 +75,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
