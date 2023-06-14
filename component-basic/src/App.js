288a591bbdedfc3fda209d633741fbcdf5fad6fe
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-14 13:54:46 +0530

    outsourcing concept component

--- component-basic/src/App.js
+++ component-basic/src/App.js
@@ -2,6 +2,7 @@ import componentsImage from "./assets/images/components.png";
 import stateImage from "./assets/images/state.png";
 import eventsImage from "./assets/images/events.png";
 import Header from "./components/Header/Header";
+import Concept from "./components/Concept/Concept";
 
 const concepts = [
   {
@@ -29,21 +30,21 @@ function App() {
     <div>
       <Header />
       <ul id="concepts">
-        <li className="concept">
-          <img src={concepts[0].image} alt="TODO: TITLE" />
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[1].image} alt="TODO: TITLE" />
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[2].image} alt="TODO: TITLE" />
-          <h2>{concepts[2].title}</h2>
-          <p>{concepts[2].description}</p>
-        </li>
+        <Concept
+          image={concepts[0].image}
+          title={concepts[0].title}
+          description={concepts[0].description}
+        />
+        <Concept
+          image={concepts[1].image}
+          title={concepts[1].title}
+          description={concepts[1].description}
+        />
+        <Concept
+          image={concepts[2].image}
+          title={concepts[2].title}
+          description={concepts[2].description}
+        />
       </ul>
     </div>
   );
