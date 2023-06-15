cf239e75d1957bdf29f061cda36b4958f2bdfa20
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-16 00:08:21 +0530

    using react context api

--- side-effects--reducers--context-api/src/App.js
+++ side-effects--reducers--context-api/src/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
+import AuthContext from "./store/auth-context";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
@@ -27,13 +28,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
