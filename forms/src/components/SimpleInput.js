4615fbf8794f06c870bcbb6549f0b46ffe7cf4c1
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-23 18:37:09 +0530

    reading user inputs & form submission

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ""; // Not ideal, don't manipulate the DOM
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
