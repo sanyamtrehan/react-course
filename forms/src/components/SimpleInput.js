5358599a28a50fb2dcebc103abfb516c34e3b9ae
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-23 19:10:58 +0530

    managing overall form validity

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -7,6 +7,11 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputHandler = (event) => {
     setEnteredName(event.target.value);
 
@@ -52,7 +57,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
