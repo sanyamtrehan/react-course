2d7e66be8ef4d9ffdb8bd8d898ee466ea5d2a7eb
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-26 02:07:38 +0530

    fixing state key

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { number: 0 }, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return { counter: state.counter + 1 };
   }
