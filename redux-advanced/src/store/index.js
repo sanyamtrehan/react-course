4460b17eb81210d0fdc27344bf0689824c1d0b68
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-07-01 17:12:47 +0530

    adding ui slice and cart slice

---
+++ redux-advanced/src/store/index.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import uiSliceReducer from "./ui-slice";
+
+const store = configureStore({ reducer: { ui: uiSliceReducer } });
+
+export default store;
