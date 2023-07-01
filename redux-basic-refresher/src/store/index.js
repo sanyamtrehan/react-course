9e00df923f054082206f74b98fbcf29adb8547ff
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-07-01 17:49:49 +0530

    adding cart slice

--- redux-basic-refresher/src/store/index.js
+++ redux-basic-refresher/src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiSliceReducer from "./ui-slice";
+import cartSliceReducer from "./cart-slice";
 
-const store = configureStore({ reducer: { ui: uiSliceReducer } });
+const store = configureStore({
+  reducer: { ui: uiSliceReducer, cart: cartSliceReducer },
+});
 
 export default store;
