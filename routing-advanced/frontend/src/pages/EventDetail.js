883bfa48c390e3164ba570ca93b5466776691c06
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-07-30 14:01:45 +0530

    using useRouterLoaderData and edit event page

--- routing-advanced/frontend/src/pages/EventDetail.js
+++ routing-advanced/frontend/src/pages/EventDetail.js
@@ -1,8 +1,9 @@
-import { json, useLoaderData } from "react-router-dom";
+import { json, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  const data = useLoaderData();
+  const data = useRouteLoaderData("event-detail");
+  console.log(data);
 
   return <EventItem event={data.event} />;
 }
